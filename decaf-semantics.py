3b1a3ff1b21b0239d90fd5d9484e67b1f20af9ce
Jack <jackowenpro@gmail.com>
2020-10-13 14:41:57 +0100

    Added semantic rule to language
    Added semantic rule to the Decaf Language: “No identifier is declared twice in the same scope”

--- decaf-semantics.py
+++ decaf-semantics.py
@@ -7,8 +7,33 @@ from SymbolTable import HEAP, STACK, SymbolTable, VarSymbol, MethodSymbol
 class DecafSemanticChecker(DecafVisitor):
     def __init__(self):
         super().__init__()
-        
-    def visitProgram(self, ctx:DecafParser.ProgramContext):
+        self.st = SymbolTable()
+        # initialise an empty Symbol Table object
+
+    def visitProgram(self, ctx: DecafParser.ProgramContext):
+        self.st.enterScope()  # enter symbol table scope
+        self.visitChildren(ctx)
+        self.st.exitScope()
+
+    def visitVar_decl(self, ctx: DecafParser.Var_declContext):
+        line_num = ctx.start.line
+        for var_decl in ctx.ID():
+            var_name = var_decl.getText() # gets the variable name (eg. x)
+            var_symbol = self.st.probe(var_name) # search Symbol Table for variable entry
+            
+            if var_symbol != None: # if variable does NOT exist in Symbol Table
+                print('Error on line', line_num, 'variable \'', var_name,
+                    '\' already declared on line', var_symbol.line)
+            else:
+                var_symbol = VarSymbol(id=var_name,
+                                    type='int',
+                                    line=line_num,
+                                    size=8,
+                                    mem=STACK)
+                self.st.addSymbol(
+                    var_symbol
+                )  # add var_symbol to the scope (st abbreviation of SymbolTable)
+
         return self.visitChildren(ctx)
 
 filein = open('testdata/semantics/illegal-01.dcf', 'r')

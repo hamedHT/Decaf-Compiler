22a7792b9ae3876c1d74d9cb838513ba72b7a44d
Jack <jackowenpro@gmail.com>
2020-10-14 11:43:02 +0100

    Added semantic rule
    added semantic rule "int_literal in an array declaration must be greater than 0".
    added semantic rules todo checklist to README

--- README.md
+++ README.md
@@ -5,10 +5,35 @@ This project is practice for building a Compiler using Antlr4
 ## Parser (Java)
 
 `antlr4 Decaf.g4`
+
 `javac Decaf*.java` <!-- compiles java bytecode files -->
+
 `grun Decaf program -gui testdata/parser/legal-01`
 
 ## Parser (with Python target modules)
 
 `antlr4 Decaf.g4 -Dlanguage=Python3`
+
 `antlr4 Decaf.g4 -Dlanguage=Python3 -visitor` <!-- Creates a template Vistor python file with a template Class of Antlr objects -->
+
+## Decaf Compiler Semantic rules todo
+- [x] (1) No identifier is declared twice in the same scope.
+- [x] (2) No identifier is used before it is declared.
+- [ ] (3) The program contains a definition for a method called main that has no parameters (note that since execution starts at method main, any methods defined after main will never be executed).
+- [x] (4) The int_literal in an array declaration must be greater than 0.
+- [ ] (5) The number and types of arguments in a method call must be the same as the number and types of the formals, i.e., the signatures must be identical.
+- [ ] (6) If a method call is used as an expression, the method must return a result.
+- [ ] (7) A return statement must not have a return value unless it appears in the body of a method that is declared to return a value.
+- [ ] (8) The expression in a return statement must have the same type as the declared result type of the enclosing method definition.
+- [ ] (9) An id used as a location must name a declared local/global variable or formal parameter. 
+- [ ] (10) For all locations of the form id[expr] 
+  - [ ] id must be an array variable
+  - [ ] the type of expr must be int
+- [ ] (11) The expr in an if statement must have type boolean
+- [ ] (12) The operands of arith_op and rel_op must have type int.
+- [ ] (13) The operands of eq_op must have the same type, either int or boolean
+- [ ] (14) The operands of cond_op and logical not ! must have type boolean
+- [ ] (15) The location and the expr in an assignment, location = expr, must have the same type.
+- [ ] (16) The location and the expr in an incrementing/decrementing assignment, location += expr and location --= expr, must be of type
+- [ ] (17) The initial expr and the ending expr of for must have type int.
+- [ ] (18) Break and continue statements must be within the body of a for.
--- decaf-semantics.py
+++ decaf-semantics.py
@@ -4,6 +4,7 @@ from DecafParser import DecafParser
 from DecafVisitor import DecafVisitor
 from SymbolTable import HEAP, STACK, SymbolTable, VarSymbol, MethodSymbol
 
+
 class DecafSemanticChecker(DecafVisitor):
     def __init__(self):
         super().__init__()
@@ -11,7 +12,7 @@ class DecafSemanticChecker(DecafVisitor):
         # initialise an empty Symbol Table object
 
     def visitProgram(self, ctx: DecafParser.ProgramContext):
-        self.st.enterScope() # enter symbol table scope
+        self.st.enterScope()  # enter symbol table scope
         self.visitChildren(ctx)
         self.st.exitScope()
 
@@ -19,26 +20,26 @@ class DecafSemanticChecker(DecafVisitor):
         # semantic rule: No identifier is declared twice in the same scope
         line_num = ctx.start.line
         for var_decl in ctx.ID():
-            var_name = var_decl.getText() # gets the variable name (eg. x)
-            var_symbol = self.st.probe(var_name) # search Symbol Table for variable entry
-            
-            if var_symbol != None: # if variable does NOT exist in Symbol Table
+            var_name = var_decl.getText()  # gets the variable name (eg. x)
+            var_symbol = self.st.probe(
+                var_name)  # search Symbol Table for variable entry
+
+            if var_symbol != None:  # if variable does NOT exist in Symbol Table
                 print('Error on line', line_num, 'variable \'', var_name,
-                    '\' already declared on line', var_symbol.line)
+                      '\' already declared on line', var_symbol.line)
             else:
                 var_symbol = VarSymbol(id=var_name,
-                                    type='int',
-                                    line=line_num,
-                                    size=8,
-                                    mem=STACK)
+                                       type='int',
+                                       line=line_num,
+                                       size=8,
+                                       mem=STACK)
                 self.st.addSymbol(
                     var_symbol
                 )  # add var_symbol to the scope (st abbreviation of SymbolTable)
 
         return self.visitChildren(ctx)
 
-    # attempt to check all statement rules for a location symbol table check
-    def visitStatement(self, ctx:DecafParser.StatementContext):
+    def visitStatement(self, ctx: DecafParser.StatementContext):
         # semantic rule: No identifier is used before it is declared
         if ctx.location() != None:
             line_num = ctx.start.line
@@ -52,8 +53,28 @@ class DecafSemanticChecker(DecafVisitor):
 
         self.visitChildren(ctx)
 
+    # warn the user that any method defined after the main method will never be executed.
 
-filein = open('testdata/semantics/illegal-02.dcf', 'r')
+    # semantic rule: int_literal in an array declaration must be greater than 0
+    def visitField_name(self, ctx: DecafParser.Field_nameContext):
+        if ctx.int_literal() != None:
+            if int(ctx.int_literal().DECIMAL_LITERAL().getText()) < 1:
+                line_num = ctx.start.line
+                var_name = ctx.ID().getText()
+                print("Error on line", line_num, "variable '", var_name,
+                      "' array size must be greater than 0")
+
+        return self.visitChildren(ctx)
+
+    # semantic rule: number and types of arguments in a method call must be the same as 
+    #   the number and types of the formals, i.e., the signatures must be identical.
+    # 1. check lengths of method_call params === method_decl params
+    def visitMethod_call(self, ctx:DecafParser.Method_callContext):
+        print(len(ctx.expr()))
+        return self.visitChildren(ctx)
+
+
+filein = open('testdata/semantics/illegal-05.dcf', 'r')
 lexer = DecafLexer(ant.InputStream(filein.read()))
 
 stream = ant.CommonTokenStream(lexer)
